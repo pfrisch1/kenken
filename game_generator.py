4db9579d0cb91c79fa2d1b66c2d90ddf2fc824fb
Peter Frisch <peter_frisch@brown.edu>
2018-08-30 23:55:29 -0400

    game generation works

--- game_generator.py
+++ game_generator.py
@@ -1,12 +1,59 @@
+from collections import defaultdict
+from Game import *
+import copy
 import random
+def getBoard(answer, counts, nClues, gameSize):
+    available = []
+    for r in range(gameSize):
+        for c in range(gameSize):
+            available.append((r,c))
 
-# Get a list of randomly generated gameSize x gameSize sudoku boards
-def getSudokuValidBoards(nBoards, gameSize):
-    initialBoard = initBoard(gameSize)
-    return [shuffleBoard(initialBoard) for _ in xrange(nBoards)]
+    clueList = []
+
+    while available:
+        (op, length) = randomClue(counts, nClues, ['/'])
+        cells = getClueCells(available, length, gameSize)
+        if len(cells) == 1:
+            op = EQL
+        val = obtainValue(cells, op, answer)
+        clueList.append(Clue(val,op,cells))
+
+    return Game(clueList, answer, gameSize)
+
+def makeBoards(nBoards, gameSize):
+    counts = defaultdict(lambda: defaultdict(int))
+    games = getGamesFromXML('data/gamedata%d.xml' % gameSize, gameSize)
+    nClues = 0
+    for game in games:
+        for clue in game.clues:
+            counts[clue.operation][clue.length] += 1
+            nClues += 1
+
+    boards = []
+
+    answers = getSudokuValidBoards(nBoards, gameSize)
+    
+    for answer in answers:
+        boards.append(getBoard(answer, counts, nClues, gameSize))
+
+    return boards
+
+def getClueCells(available, desiredLength, gameSize):
+    cells = [available.pop(0)]
+    currentCell = cells[-1]
+
+    while len(cells) < desiredLength:
+        neighbors = getAvailableNeighbors(currentCell, available, gameSize)
+        if not neighbors:
+            break
+        currentCell = random.choice(neighbors)
+        cells.append(currentCell)
+        available.remove(currentCell)
+
+    return cells
 
 # Initialize a known-valid sudoku board deterministically
-def _initBoard(n):
+def initBoard(n):
     nums = [e + 1 for e in range(n)]
     init = [nums]
     for i in range(1, n):
@@ -14,12 +61,102 @@ def _initBoard(n):
         init.append(row)
     return init
 
+# Get a list of randomly generated gameSize x gameSize sudoku boards
+def getSudokuValidBoards(nBoards, gameSize):
+    initialBoard = initBoard(gameSize)
+    return [shuffleBoard(initialBoard) for _ in xrange(nBoards)]
+
 # Shuffle rows and columns of an n x n list of lists
-def _shuffleBoard(board):
+def shuffleBoard(board):
     random.shuffle(board)
     boardT = transposeBoard(board)
     random.shuffle(boardT)
     return transposeBoard(boardT)
 
-def _transposeBoard(board):
+def transposeBoard(board):
     return [list(*row) for row in zip(zip(*board))]
+
+def obtainValue(cells, operation, answer):
+    # cells: list of tuples
+    # operation: string
+    # answer: matrix of numbers
+    if operation is EQL:
+        (r,c) = cells[0]
+        val = answer[r][c]
+    elif operation is ADD:
+        val = 0
+        for cell in cells:
+            (r,c) = cell
+            val += answer[r][c]
+    elif operation is MUL:
+        val = 1
+        for cell in cells:
+            (r,c) = cell
+            val *= answer[r][c]
+    elif operation is SUB:
+        (r1, c1) = cells[0]
+        (r2, c2) = cells[1]
+        val = answer[r1][c1] - answer[r2][c2]
+        if val < 0:
+            val = -val
+    elif operation is DIV:
+        (r1, c1) = cells[0]
+        (r2, c2) = cells[1]
+        val = answer[r1][c1] / float(answer[r2][c2])
+        if val < 1:
+            val = 1 / val
+    return val
+
+
+def getAvailableNeighbors(cell, available, gameSize):
+    # cell: tuple
+    # available: set of cells
+    # gameSize: width of board
+    neighbors = []
+    (r,c) = cell
+    if r > 0:
+        if (r - 1, c) in available:
+            neighbors.append((r - 1, c))
+    if r < gameSize - 1:
+        if (r + 1, c) in available:
+            neighbors.append((r + 1, c))
+    if c > 0:
+        if (r, c - 1) in available:
+            neighbors.append((r, c - 1))
+    if c < gameSize - 1:
+        if (r, c + 1) in available:
+            neighbors.append((r, c + 1))
+
+    return neighbors
+
+def findDivisionSpots(answer, gameSize):
+    # answer: matrix of numbers
+    # gamesize: width of board
+    spots = []
+    for r in range(gameSize):
+        for c in range(gameSize):
+            val1 = float(answer[r][c])
+            if r < gameSize - 1:
+                val2 = float(answer[r+1][c])
+                if (val1 / val2 % 0) or (val2 / val1 % 0):
+                    spots.append([(r,c),(r+1,c)])
+            if c < gameSize - 1:
+                val2 = float(answer[r][c+1])
+                if (val1 / val2 % 0) or (val2 / val1 % 0):
+                    spots.append([(r,c),(r,c+1)])
+    return spots
+
+
+def randomClue(inputCounts,nClues,exclude=[]):
+    # inputCounts is a doubledict [operation][length]
+    counts = copy.deepcopy(inputCounts)
+    for op in exclude:
+        for length in counts[op]:
+            nClues -= counts[op][length]
+        counts[op] = defaultdict(int) # reset to zero
+    clueNum = random.randrange(nClues)
+    for op in counts:
+        for length in counts[op]:
+            if clueNum < counts[op][length]:
+                return (op,length)
+            clueNum -= counts[op][length]
